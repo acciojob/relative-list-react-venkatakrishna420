01d0f06bc94a1069f1edaa6b50e8603489cad4d8
KAKARLA VENKATA KRISHNA <kakarlavenkatakrishna9@gmail.com>
2025-10-26 21:11:41 +0530

    latest change

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,25 @@ import React from 'react'
 const App = () => {
   return (
     <div id="main">
+    <h1>Relative List</h1>
+      <Relatives />
                {/* Do not remove the main div */}
     </div>
   )
 }
 
 export default App
+function Relatives(){
+    const relatives = ['Alice', 'Bob', 'Charlie', 'Diana','venkat'];
+    
+    return(
+        <div>
+            <ol key="relativeList">
+                {relatives.map((relatives,index)=>(
+                    <li key={`relativeListItem${index + 1}`}>{relatives}</li>
+                ))}
+               
+            </ol>
+        </div>
+       )
+    }
